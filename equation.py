e8a3fc52b969783ddbca19b3c56cf50cc72dd7e7
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2023-12-06 20:23:05 -0700

    initial commit

---
+++ equation.py
@@ -0,0 +1,29 @@
+# TIME
+day = 1
+hour = 24
+minute = 60
+second = 60
+
+# HEAVEN TIME
+heaven_day = day
+heaven_hour = heaven_day * hour
+heaven_minute = heaven_hour * minute
+heaven_second = heaven_minute * second
+
+# HEAVEN RESULTS
+print('Day in heaven: ' + '{:,}'.format(heaven_day))
+print('Hours in heaven: ' + '{:,}'.format(heaven_hour))
+print('Minutes in heaven: ' + '{:,}'.format(heaven_minute))
+print('Seconds in heaven: ' + '{:,}'.format(heaven_second))
+
+# EARTH TIME
+earth_day = day * 365
+earth_hour = earth_day * hour
+earth_minute = earth_hour * minute
+earth_second = earth_minute * second
+
+# EARTH RESULTS
+print('Days on earth: ' + '{:,}'.format(earth_day))
+print('Hours on earth: ' + '{:,}'.format(earth_hour))
+print('Minutes on earth: ' + '{:,}'.format(earth_minute))
+print('Seconds on earth: ' + '{:,}'.format(earth_second))
